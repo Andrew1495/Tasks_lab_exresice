dd4f4518f6be84af60c7a4c29d33578bd7fa5d8d
benjaminsal92@gmail.com <benjaminsal92@gmail.com>
2022-06-23 16:00:36 +0100

    worked on imput file and updated app

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from module.output import *
 from data.task_list import *
 from module.imput import *
 
+
 while (True):
     print_menu()
-    
+    option = ask_selection_option()
     if (option.lower() == 'q'):
         break
     if option == '1':
@@ -16,7 +17,7 @@ while (True):
     elif option == '3':
         print_list(get_completed_tasks(tasks))
     elif option == '4':
-        
+        description = ask_task_desciption()
         task = get_task_with_description(tasks, description)
         if task is not None:
             mark_task_complete(task)
@@ -24,14 +25,14 @@ while (True):
         else:
             print("Task not found")
     elif option == '5':
-        
+        time = ask_duration()
         print_list(get_tasks_taking_at_least(tasks, time))
     elif option == '6':
-        
+        ask_for_description
         print(get_task_with_description(tasks, description))
     elif option == '7':
-        
-        
+        description = ask_for_description()
+        time_taken = ask_time()
         task = create_task(description, time_taken)
         tasks.append(task)
     else:
--- module/imput.py
+++ module/imput.py
@@ -1,6 +1,20 @@
-option = input("Select an option 1, 2, 3, 4, 5, display (m)enu or (q)uit: ")
-description = input("Enter task description to search for: ")
-time = int(input("Enter task duration: "))
-description = input("Enter task description to search for: ")
-description = input("Enter description: ")
-time_taken = int(input("Enter time taken: "))
+def ask_selection_option():
+    option = input("Select an option 1, 2, 3, 4, 5, display (m)enu or (q)uit: ")
+    return option
+
+def ask_task_desciption():
+    description = input("Enter task description to search for: ")
+    return description
+
+def ask_duration():
+    time = int(input("Enter task duration: "))
+    return time
+
+# description = input("Enter task description to search for: ")
+def ask_for_description():
+    description = input("Enter description: ")
+    return description
+
+def ask_time():
+    time_taken = int(input("Enter time taken: "))
+    return time_taken
